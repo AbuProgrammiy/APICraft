df929bb5c7dc705183128b2bdf359c9639ae226f
Abu_Programmiy <abdukholiq23@gmail.com>
2024-08-07 16:16:35 +0500

    before accurency

--- src/app/components/flexible-table/flexible-table.component.ts
+++ src/app/components/flexible-table/flexible-table.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, Renderer2 } from '@angular/core';
 import { CRUDService } from '../../services/CRUD/crud.service';
 import { jwtDecode } from 'jwt-decode';
+import { environment } from '../../../environments/environment.development';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'flexible-table',
@@ -11,46 +13,45 @@ export class FlexibleTableComponent {
   @Input() tableName!: string;
   @Input() columns!: string[];
 
-  constructor(private renderer: Renderer2, private crudService: CRUDService) {
-  }
+  constructor(private renderer: Renderer2, private crudService: CRUDService,private messageService:MessageService) { }
 
-  isGettingAll: boolean = false
+  isLoading: boolean = false
   isGettingByAny: boolean = false
   isCreating: boolean = false
   isUpdating: boolean = false
   isDeleting: boolean = false
-  securityKey: string = (jwtDecode(localStorage.getItem("accessToken")!) as any).SecurityKey
-  page!: number
-  count!: number
+  inputSecurityKey: string = (jwtDecode(localStorage.getItem("accessToken")!) as any).SecurityKey
+  inputTableName=this.tableName
+  inputPage!: number
+  inputCount!: number
 
   executeGetAll() {
-    console.log(this.page);
+    this.isLoading = true
 
     const responseHandler = document.getElementById("response-handler")!
-    const requestUrlSign=this.renderer.createElement("p")
-    const requestUrlHandler=this.renderer.createElement("p")
+    const requestUrlSign = this.renderer.createElement("p")
+    const requestUrlHandler = this.renderer.createElement("p")
     const jsonHandler = this.renderer.createElement("pre")
     const responseSign = this.renderer.createElement("p")
 
-    jsonHandler.className = "json-handler"
     requestUrlHandler.className = "request-url-handler"
+    jsonHandler.className = "json-handler"
 
-    requestUrlSign.innerHTML="Request URL"
-    requestUrlHandler.innerHTML="path:"
-    responseHandler.innerHTML=""
-    responseSign.innerHTML="Response body"
+    requestUrlSign.innerHTML = "Request URL"
+    requestUrlHandler.innerHTML = encodeURI(`${environment.baseUrl}CRUD/GetAll/${this.inputSecurityKey}/${this.inputTableName}${this.inputPage != undefined && this.inputPage != null && this.inputCount != undefined && this.inputCount != null ? `?page=${this.inputPage}&count=${this.inputCount}` : ""}`)
+    responseHandler.innerHTML = ""
+    responseSign.innerHTML = "Response body"
 
     responseHandler.appendChild(requestUrlSign)
     responseHandler.appendChild(requestUrlHandler)
     responseHandler.appendChild(this.renderer.createElement("hr"))
     responseHandler.appendChild(responseSign)
 
-    this.crudService.getAll(this.securityKey, this.tableName, this.page, this.count).subscribe({
+    this.crudService.getAll(this.inputSecurityKey, this.inputTableName, this.inputPage, this.inputCount).subscribe({
       next: (response) => {
-        console.log(response[0]);
         if (Array.isArray(response)) {
 
-          jsonHandler.appendChild(this.renderer.createText(`[${response.length!=0?"\n":""}`))
+          jsonHandler.appendChild(this.renderer.createText(`[${response.length != 0 ? "\n" : ""}`))
 
           for (let i = 0; i < response.length; i++) {
 
@@ -60,25 +61,43 @@ export class FlexibleTableComponent {
 
             for (let j = 0; j < keys.length; j++) {
               jsonHandler.appendChild(this.renderer.createText(`    "${keys[j]}":`))
-              const value=this.renderer.createElement("span")
-              value.className="response-value"
-              value.innerHTML=` "${values[j]}"${j+1!=keys.length?",":""}\n`
+              const value = this.renderer.createElement("span")
+              value.className = "response-value"
+              value.innerHTML = ` "${values[j]}"${j + 1 != keys.length ? "," : ""}\n`
               jsonHandler.appendChild(value)
             }
 
-            jsonHandler.appendChild(this.renderer.createText(`  }${i+1!=response.length?",":""}\n`))
+            jsonHandler.appendChild(this.renderer.createText(`  }${i + 1 != response.length ? "," : ""}\n`))
           }
           jsonHandler.appendChild(this.renderer.createText("]"))
         }
         else {
+          jsonHandler.appendChild(this.renderer.createText("{\n"))
+          const keys=Object.keys(response)
+          const values=Object.values(response)
 
+          for (let j = 0; j < keys.length; j++) {
+            jsonHandler.appendChild(this.renderer.createText(`    "${keys[j]}":`))
+            const value = this.renderer.createElement("span")
+            value.className = "response-value"
+            value.innerHTML = ` "${values[j]}"${j + 1 != keys.length ? "," : ""}\n`
+            jsonHandler.appendChild(value)
+          }
+
+          jsonHandler.appendChild(this.renderer.createText("}"))     
         }
 
         responseHandler.appendChild(jsonHandler)
+        this.isLoading = false
       },
       error: (err) => {
-        console.log(err);
+        this.messageService.add({ severity: 'contrast', summary: 'Error', detail: 'Something went wrong!' });
+        this.isLoading = false
       }
     })
+  }
+
+  presentResponse(){
+    
   }
 }
